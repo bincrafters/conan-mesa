fdb69f1f64bdbb38a7d0dc4253c1251d81d68076
Eric Lemanissier <eric.lemanissier@gmail.com>
2020-01-08 21:08:19 +0100

    build libcxx variants

--- build.py
+++ build.py
@@ -5,7 +5,7 @@ from conans.client.build.cppstd_flags import cppstd_default
 
 if __name__ == "__main__":
 
-    builder = build_template_default.get_builder(pip_install=["Mako"])
+    builder = build_template_default.get_builder(pip_install=["Mako"], pure_c=False)
     builder.update_build_if(lambda build: cppstd_default(build.settings['compiler'], build.settings['compiler.version']).endswith('98'),
         new_settings = {'compiler.cppstd': '11'})
     builder.run()
