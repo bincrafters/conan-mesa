d560070f983f229020768ab218471d479c72ccbd
Eric Lemanissier <eric.lemanissier@gmail.com>
2020-04-04 00:37:42 +0200

    fix CI
    by not using non documented conan feature

--- build.py
+++ build.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from bincrafters import build_template_default
-from conans.client.build.cppstd_flags import cppstd_default
+from conans import tools
 import os
 
 if __name__ == "__main__":
@@ -10,6 +10,8 @@ if __name__ == "__main__":
         docker_entry_script = "conan install ninja/1.9.0@ --build ninja"
 
     builder = build_template_default.get_builder(pip_install=["Mako"], pure_c=False, docker_entry_script=docker_entry_script)
-    builder.update_build_if(lambda build: cppstd_default(build.settings['compiler'], build.settings['compiler.version']).endswith('98'),
+    builder.update_build_if(lambda build:\
+        (build.settings['compiler'] == 'gcc' and tools.Version(build.settings['compiler.version']) < "6") or \
+        (build.settings['compiler'] == 'clang' and tools.Version(build.settings['compiler.version']) < "6"),
         new_settings = {'compiler.cppstd': '11'})
     builder.run()
