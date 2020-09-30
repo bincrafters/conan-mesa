1dc76abedb24ce7e4ddf782e04c15023b79531c3
bincrafters-user <bincrafters@gmail.com>
2020-09-30 14:08:07 +0000

    Update Conan conventions
    Automatically created by bincrafters-conventions 0.26.1

--- build.py
+++ build.py
@@ -7,7 +7,7 @@ import os
 if __name__ == "__main__":
     docker_entry_script = None
     if os.getenv("CONAN_GCC_VERSIONS") == "4.9":
-        docker_entry_script = "conan install ninja/1.10.0@ --build ninja"
+        docker_entry_script = "conan install ninja/1.10.1@ --build ninja"
 
     builder = build_template_default.get_builder(pip_install=["Mako"], pure_c=False, docker_entry_script=docker_entry_script)
     builder.update_build_if(lambda build:\
